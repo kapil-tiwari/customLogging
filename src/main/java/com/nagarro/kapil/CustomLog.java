f658092eb7e3bd87108641252e4e77127896bfbb
Kapil Tiwari <kapil.tiwari@nagarro.com>
2021-05-20 18:37:57 +0530

    added customLog class feature

--- src/main/java/com/nagarro/kapil/CustomLog.java
+++ src/main/java/com/nagarro/kapil/CustomLog.java
@@ -5,8 +5,8 @@ import org.apache.logging.log4j.Logger;
 
 public class CustomLog {
 
-	private static final Logger logger = LogManager.getLogger(CustomLog.class);
-
+	 private static final Logger log = LogManager.getLogger(CustomLog.class);
+     private static final CustomLogger logger =  CustomLogger.getLogger(CustomLog.class);
 	public static void main(String[] args) {
 		
 		User user = new User(1, "Kapil", 24);
@@ -14,6 +14,8 @@ public class CustomLog {
 		logger.debug("Debug log");
 		logger.info("User details : {}", user);
 		logger.warn("We've just greeted the user!");
+		logger.audit("My audit logger");
+		logger.error("Error message");
 
 	}
 
